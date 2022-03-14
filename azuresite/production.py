538ccd80f8e7767de758254a7f838dd0f959394c
niarferuto <ing.efrain.orjuela@gmail.com>
2022-03-13 22:53:21 -0500

    Update production.py

--- azuresite/production.py
+++ azuresite/production.py
@@ -26,14 +26,4 @@ hostname = os.environ['DBHOST']
 
 # Configure Postgres database; the full username is username@servername,
 # which we construct using the DBHOST value.
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DBNAME'],
-        'HOST': hostname + ".postgres.database.azure.com",
-        'USER': os.environ['DBUSER'] + "@" + hostname,
-        'PASSWORD': os.environ['DBPASS'] 
-    }
-}
-"""
+
